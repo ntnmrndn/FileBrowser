994cc479451a5c3a026bbc4f145466dcd4afd55c
Antoine Marandon <antoine@marandon.fr>
2023-07-19 17:45:24 +0900

    Update function

--- FileBrowser/PreviewTransitionViewController.swift
+++ FileBrowser/PreviewTransitionViewController.swift
@@ -11,17 +11,17 @@ import QuickLook
 
 /// Preview Transition View Controller was created because of a bug in QLPreviewController. It seems that QLPreviewController has issues being presented from a 3D touch peek-pop gesture and is produced an unbalanced presentation warning. By wrapping it in a container, we are solving this issue.
 class PreviewTransitionViewController: UIViewController {
-    
-    @IBOutlet weak var containerView: UIView!
-    
-    let quickLookPreviewController = QLPreviewController()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-      self.addChildViewController(quickLookPreviewController)
-        containerView.addSubview(quickLookPreviewController.view)
-        quickLookPreviewController.view.frame = containerView.bounds
-      quickLookPreviewController.didMove(toParentViewController: self)
-    }
-
+  
+  @IBOutlet weak var containerView: UIView!
+  
+  let quickLookPreviewController = QLPreviewController()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.addChild(quickLookPreviewController)
+    containerView.addSubview(quickLookPreviewController.view)
+    quickLookPreviewController.view.frame = containerView.bounds
+    quickLookPreviewController.didMove(toParent: self)
+  }
+  
 }
