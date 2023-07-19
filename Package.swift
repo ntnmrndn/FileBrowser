9fdffe3d1bcce494be09f5b5c97ed34aecc1a026
Antoine Marandon <antoine@marandon.fr>
2023-07-19 18:59:15 +0900

    Fix package.swift

--- Package.swift
+++ Package.swift
@@ -4,39 +4,39 @@
 import PackageDescription
 
 let package = Package(
-    name: "FileBrowser",
-     platforms: [
-        .iOS(.v14)
-    ],
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "FileBrowser",
-            targets: ["FileBrowser"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "FileBrowser",
-            dependencies: [
-        .process("./documents@2x.png"),
-        .process("./files@2x.png"),
-        .process("./FileBrowser.xib"),
-        .process("./folder@2x.png"),
-        .process("./image@2x.png"),
-        .process("./pdf@2x.png"),
-        .process("./zip@2x.png"),
-        .process("./PreviewTransitionViewController.xib"),
-        .process("./WebviewPreviewViewContoller.xib"),
-        ],
-            path: "FileBrowser",
-            exclude:[]
-            ),
-    ],
-    swiftLanguageVersions: [.v5]
+  name: "FileBrowser",
+  platforms: [
+    .iOS(.v14)
+  ],
+  products: [
+    // Products define the executables and libraries a package produces, and make them visible to other packages.
+    .library(
+      name: "FileBrowser",
+      targets: ["FileBrowser"]),
+  ],
+  dependencies: [
+    // Dependencies declare other packages that this package depends on.
+    // .package(url: /* package url */, from: "1.0.0"),
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+    // Targets can depend on other targets in this package, and on products in packages this package depends on.
+    .target(
+      name: "FileBrowser",
+      path: "FileBrowser",
+      exclude: ["examples", "FileBrowserTests", "build.sh", "FileBrowser.podspec"],
+      resources: [
+        .copy("./documents@2x.png"),
+        .copy("./files@2x.png"),
+        .copy("./FileBrowser.xib"),
+        .copy("./folder@2x.png"),
+        .copy("./image@2x.png"),
+        .copy("./pdf@2x.png"),
+        .copy("./zip@2x.png"),
+        .copy("./PreviewTransitionViewController.xib"),
+        .copy("./WebviewPreviewViewContoller.xib")
+      ]
+    )
+  ],
+  swiftLanguageVersions: [.v5]
 )
